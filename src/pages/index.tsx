2859854a20dd1cf041de54e9e56ec03cc3753953
Jed Borseth <jedborseth@Jeds-MBP.va.shawcable.net>
2023-11-12 15:34:38 -0800

    base head component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import BaseHead from "~/components/BaseHead";
 import { Card } from "~/components/Card";
 
 export default function Home() {
   return (
     <div>
+      <BaseHead />
       <Card type="number" color="red" num="5" />
     </div>
   );
