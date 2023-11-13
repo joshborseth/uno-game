04b879d914c9e80738a9083a3cfc72da6929bb16
Jed Borseth <jedborseth@Jeds-MBP.va.shawcable.net>
2023-11-12 16:46:20 -0800

    removed the card from the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,6 @@ export default function Home() {
   return (
     <>
       <BaseHead />
-      <div className="flex min-h-screen w-full items-center justify-center">
-        <Card type="wild" />
-      </div>
     </>
   );
 }
