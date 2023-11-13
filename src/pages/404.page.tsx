66a674d22374b3e808bf74c3defec414dd7c584e
Jed Borseth <jedborseth@Jeds-MBP.va.shawcable.net>
2023-11-13 11:08:33 -0800

    styles n stuff

--- src/pages/404.page.tsx
+++ src/pages/404.page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Page404 = () => {
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center bg-slate-100">
-      <h1 className="text-[12rem] font-extrabold tracking-widest text-primary">
+      <h1 className="text-[11rem] font-extrabold tracking-widest text-primary">
         404
       </h1>
       <div className="absolute rotate-12 rounded bg-secondary px-4 py-2 text-sm">
@@ -16,7 +16,7 @@ const Page404 = () => {
         >
           <span className="absolute inset-0 translate-x-1 translate-y-1 bg-primary transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
 
-          <span className="relative block border border-current bg-accent px-8 py-3 font-bold">
+          <span className="relative block border border-current bg-neutral-content px-8 py-3 font-bold">
             Return Home
           </span>
         </Link>
