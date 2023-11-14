69911ff869fe4a86e28d46b8938c1f22d25d8861
joshborseth <joshuaborseth@gmail.com>
2023-11-13 17:53:47 -0800

    boomshackalaka

--- src/pages/player/waiting-room/[code].page.tsx
+++ src/pages/player/waiting-room/[code].page.tsx
@@ -46,15 +46,16 @@ const WaitingRoom = () => {
   return (
     <>
       <BaseHead title="UNO - Player Waiting Room" />
-    <main className="flex h-screen w-screen flex-wrap items-center justify-center">
-      <h1 className="text-5xl font-extrabold">Sit Tight!</h1>
-      <h2 className="w-full text-center text-4xl font-bold">
-        Your Name: <span className="text-primary font-black">{name}</span>
-      </h2>
-      <Link href="/" className="underline">
-        Change Name?
-      </Link>
-    </main>
+      <main className="flex h-screen w-screen flex-wrap items-center justify-center">
+        <h1 className="text-5xl font-extrabold">Sit Tight!</h1>
+        <h2 className="w-full text-center text-4xl font-bold">
+          Your Name: <span className="text-primary font-black">{name}</span>
+        </h2>
+        <Link href="/" className="underline">
+          Change Name?
+        </Link>
+      </main>
+    </>
   );
 };
 
