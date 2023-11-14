cb37185d4ecde7227cd2ed2a92876c6b5e82543f
joshborseth <joshuaborseth@gmail.com>
2023-11-13 20:24:28 -0800

    cards styles fix

--- src/pages/host/play/[code].page.tsx
+++ src/pages/host/play/[code].page.tsx
@@ -1,17 +1,35 @@
 import { type NextPage } from "next";
+import { useRouter } from "next/router";
 import BaseHead from "~/components/BaseHead";
 import { PlayerCard } from "~/components/PlayerCard";
+import { api } from "~/utils/api";
 
 const Play: NextPage = () => {
+  const router = useRouter();
+  const getAllPlayers = api.player.getAll.useQuery(
+    {
+      code: router.query.code as string,
+    },
+    {
+      enabled: !!router.query.code,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    },
+  );
   return (
     <>
       <BaseHead title="UNO - Host" />
       <main className="flex min-h-screen w-full items-center justify-center">
         <div className="flex gap-10">
-          <PlayerCard name="User 1" selected cardsLeft={7} />
-          <PlayerCard name="User 1" cardsLeft={1} />
-          <PlayerCard name="User 1" cardsLeft={7} />
-          <PlayerCard name="User 1" cardsLeft={7} />
+          {getAllPlayers?.data?.length &&
+            getAllPlayers.data.map((player) => (
+              <PlayerCard
+                name={player.name ?? ""}
+                key={player.uid}
+                cardsLeft={player.cards.length}
+              />
+            ))}
         </div>
       </main>
     </>
