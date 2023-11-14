bede99759b2bb7b13a29ee56af470314cc2eef42
joshborseth <joshuaborseth@gmail.com>
2023-11-14 15:57:19 -0800

    done

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -49,10 +49,10 @@ const Home = () => {
     <>
       <BaseHead />
       <main className="flex min-h-screen flex-col items-center justify-center gap-10 py-10">
-        <h1 className="pb-10 text-7xl font-black">Uno</h1>
+        <h1 className="font- pb-10 text-7xl">Uno</h1>
 
         <section className="flex w-full flex-col items-center justify-center gap-10 px-4 sm:flex-row sm:items-start">
-          <div className="card flex flex-col gap-4 border border-primary bg-primary-content shadow-xl">
+          <div className="card border-primary bg-primary-content flex flex-col gap-4 border shadow-xl">
             <div className="card-body">
               <h3 className="card-title">Host</h3>
               <p className="font-extralight">
@@ -70,7 +70,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="card border border-primary bg-primary-content shadow-xl">
+          <div className="card border-primary bg-primary-content border shadow-xl">
             <div className="card-body flex flex-col gap-6">
               <div>
                 <h2 className="card-title">Player</h2>
