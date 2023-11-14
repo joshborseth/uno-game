5312044ba57293a538ae82b0398469e51515c5e1
Jed Borseth <jedborseth@Jeds-MBP.va.shawcable.net>
2023-11-13 17:42:46 -0800

    removed allcard array

--- src/pages/player/play/[code].page.tsx
+++ src/pages/player/play/[code].page.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import BaseHead from "~/components/BaseHead";
 import CardHand from "~/components/CardHand";
 import PickupCard from "~/components/PickupCard";
-import { allPossibleCards } from "~/utils/fullDeckOfCards";
+
 const Play = () => {
   const router = useRouter();
   const code = router.query.code as string;
@@ -12,7 +12,7 @@ const Play = () => {
       <BaseHead title={`UNO - ${code ?? ""}`} />
       <main className="flex  min-h-screen w-full flex-wrap items-center justify-center">
         <PickupCard />
-        <CardHand cardArr={[...allPossibleCards]} />
+        <CardHand cardArr={[]} />
       </main>
     </>
   );
